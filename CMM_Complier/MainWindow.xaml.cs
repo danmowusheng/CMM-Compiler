9318290f4d61f8ffd3bb5bf802cb52f32dd53eab
吴茜 <52146048+tasizhousong@users.noreply.github.com>
2020-12-16 15:40:56 +0800

    高亮
    完成高亮，注释的高亮还没有完成。

--- CMM_Complier/MainWindow.xaml.cs
+++ CMM_Complier/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CMM_Complier
 {
@@ -22,12 +13,13 @@ namespace CMM_Complier
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] keywords = { "if", "else", "while", "read", "write" };//deepBlue
+        string[] classKeyword = { "real", "int" };//purple
         public MainWindow()
         {
             InitializeComponent();
-
+            
         }
-
         private void OpenFile_Clicked(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("调用开始");
@@ -49,14 +41,13 @@ namespace CMM_Complier
         private void SaveFile_Clicked(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "CMM|*.cmm";
+            saveFileDialog.Filter = "CMM|*.cmm";       
             if (saveFileDialog.ShowDialog() == true)
             {
                 var filename = saveFileDialog.FileName;
                 SaveFile(filename, editorRichTextbox);
             }
         }
-
         private static void LoadFile(string filename, RichTextBox richTextBox)
         {
             if (string.IsNullOrEmpty(filename))
@@ -80,7 +71,6 @@ namespace CMM_Complier
             }
 
         }
-
         //保存文件
         private static void SaveFile(string filename, RichTextBox richTextBox)
         {
@@ -101,6 +91,95 @@ namespace CMM_Complier
             }
         }
 
+        private void editorRichTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
+        }
+
+        public void Find(string l, RichTextBox richBox, string keyword)
+        {
+            // 设置文字指针为Document初始位置
+            // richBox.Document.FlowDirection
+            TextPointer position = richBox.Document.ContentStart;
+            while (position != null)
+            {
+                // 向前搜索,需要内容为Text
+                if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                {
+                    // 拿出Run的Text
+                    string text = position.GetTextInRun(LogicalDirection.Forward);
+                    // 可能包含多个keyword,做遍历查找
+                    int index = 0;
+                    while (index < text.Length)
+                    {
+                        index = text.IndexOf(keyword, index);
+                        // MessageBox.Show(index.ToString());
+                        if (index == -1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            // 添加为新的Range
+                            TextPointer start = position.GetPositionAtOffset(index);
+                            TextPointer end = start.GetPositionAtOffset(keyword.Length);
+                            TextPointer end1 = start.GetPositionAtOffset(index + keyword.Length);
+                            ChangeColor(l, richBox, keyword.Length, start, end);
+
+                            index += keyword.Length;
+                        }
+                    }
+                }
+                // 文字指针向前偏移
+                position = position.GetNextContextPosition(LogicalDirection.Forward);
+            }
+        }
+
+        //改变颜色
+        public void ChangeColor(string color, RichTextBox richTextBox1, int selectLength, TextPointer tpStart, TextPointer tpOffset)
+        {
+            TextRange range = richTextBox1.Selection;
+            range.Select(tpStart, tpOffset);
+
+            // 高亮选择
+            if (color == "DeepSkyBlue")
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty,
+                new SolidColorBrush(Colors.DeepSkyBlue));
+                range.ApplyPropertyValue(TextElement.FontWeightProperty,
+                FontWeights.Bold);
+            }
+            else if(color == "purple")
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty,
+                new SolidColorBrush(Colors.Purple));
+                range.ApplyPropertyValue(TextElement.FontWeightProperty,
+                FontWeights.Bold);
+            }
+            else if(color == "")
+            {
+
+            }
+            TextPointer position = richTextBox1.Document.ContentStart;
+            TextPointer start = position.GetPositionAtOffset(2);
+            TextPointer end = richTextBox1.Document.ContentEnd;
+            TextRange range1 = richTextBox1.Selection;
+            range1.Select(end, end);
+            range1.ApplyPropertyValue(TextElement.ForegroundProperty,
+            new SolidColorBrush(Colors.Black));
+        }
+
+        private void editorRichTextbox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            foreach (string s in keywords)
+            {
+                Find("DeepSkyBlue", editorRichTextbox, s); // tx是RichTextBox控件名字
+            }
+            foreach (string s in classKeyword)
+            {
+                Find("Purple", editorRichTextbox, s); // tx是RichTextBox控件名字
+            }
+            
+        }
     }
 }
