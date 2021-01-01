9d2c42e33f0146924dd1d1c9e1613853d97efbef
LightningPan <tp1032189322@live.com>
2021-01-01 22:14:38 +0800

    细节微调

--- SyntaxAnalyzer/SyntaxAnalyzer/Program.cs
+++ SyntaxAnalyzer/SyntaxAnalyzer/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 
 namespace SyntaxAnalyzer
 {
@@ -8,32 +8,240 @@ namespace SyntaxAnalyzer
     {
         static void Main(string[] args)
         {
-            /*StreamReader sr = new StreamReader(@"C:\Users\tp103\Desktop\2.list");
+            /*StreamReader sr = new StreamReader(@"C:\Users\tp103\Desktop\aaa.list");
             String s;
             while ((s = sr.ReadLine()) != null) {
                 Console.WriteLine(s.Split(" ").Length);
             }*/
-            Analyzer a = new Analyzer(@"C:\Users\tp103\Desktop\2.list");
+            Analyzer a = new Analyzer(@"C:\Users\tp103\Desktop\aaa.list");
             Node s=a.Analyze();
-            get(s);
+            //getPostExpr(s.Next[4].Next[1].Next[1].Next[2]);
+            
+           /* ProcessCaculate(s.Next[4].Next[1].Next[1]);
+            foreach (String x in ins)
+            {
+                Console.WriteLine(x);
+            }*/
             
         }
 
-        static void get(Node s) {
+        public static List<String> ins=new List<String>();
+
+
+        static void ProcessCaculate(Node s) {
+            bool f = false;
+            getPostExpr(s.Next[0]);
+            string Target = res[0];
+            if (res.Count != 1)
+            {
+                foreach (String str in res)
+                {
+                    if (str.Equals("+"))
+                    {
+                        ins.Add("OP_ADD");
+                    }
+                    else if (str.Equals("-"))
+                    {
+                        ins.Add("OP_SUB");
+                    }
+                    else if (str.Equals("*"))
+                    {
+                        ins.Add("OP_MUL");
+                    }
+                    else if (str.Equals("/"))
+                    {
+                        ins.Add("OP_DIV");
+                    }
+                    else if (str.Equals("OP_NEG"))
+                    {
+                        ins.Add(str);
+                    }
+                    else if (str.Equals("-"))
+                    {
+                        ins.Add("OP_SUB");
+                    }
+                    else if (str.Equals("locArray"))
+                    {
+                        f = true;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            ins.Add("LD_INT " + int.Parse(str));
+                        }
+                        catch (Exception)
+                        {
+                            ins.Add("LD_VAR " + str);
+                        }
+                    }
+                }
+            }
+            getPostExpr(s.Next[2]);
+            foreach (String str in res) {
+                if (str.Equals("+")) {
+                    ins.Add("OP_ADD");
+                } else if (str.Equals("-")) {
+                    ins.Add("OP_SUB");
+                }
+                else if (str.Equals("*"))
+                {
+                    ins.Add("OP_MUL");
+                }
+                else if (str.Equals("/"))
+                {
+                    ins.Add("OP_DIV");
+                }
+                else if (str.Equals("OP_NEG"))
+                {
+                    ins.Add(str);
+                }
+                else if (str.Equals("-"))
+                {
+                    ins.Add("OP_SUB");
+                } else if (str.Equals("locArray")) {
+                    ins.Add("LD_ELE_INT");
+                } else {
+                    try {
+                        ins.Add("LD_INT "+int.Parse(str));
+                    }catch(Exception) {
+                        ins.Add("LD_VAR "+str);
+                    }
+                }
+            }
+            if (f)
+            {
+                ins.Add("ST_ADR");
+            }
+            else {
+                ins.Add("ST_VAR "+Target);
+            }
+        }
+
+        static void getPostExpr(Node s) {
+            res.Clear();
+            tmp.Clear();
+            flag = true;
+            GeneratePostExpr(s);
+            while (tmp.Count != 0) {
+                res.Add(tmp.Pop());
+            }
+        }
+
+        public static List<String> res = new List<string>();
+        public static Stack<String> tmp = new Stack<string>();
+        public static bool flag;
+        static void GeneratePostExpr(Node s) {
             if (s.Next == null)
             {
                 if (s.Type == TokenType.terminal)
                 {
-                    Console.WriteLine(s.Name);
+                    if (s.Name.Equals("("))
+                    {
+                        tmp.Push("(");
+                    }
+                    else if (s.Name.Equals(")"))
+                    {
+                        while (tmp.Count != 0 && !tmp.Peek().Equals("("))
+                        {
+                            res.Add(tmp.Pop());
+                        }
+                        tmp.Pop();
+                    }
+                    else if (s.Name.Equals("+"))
+                    {
+                        while (tmp.Count != 0)
+                        {
+                            if (tmp.Peek().Equals("(")|| tmp.Peek().Equals("["))
+                            {
+                                break;
+                            }
+                            res.Add(tmp.Pop());
+                        }
+                        tmp.Push("+");
+                    }
+                    else if (s.Name.Equals("-"))
+                    {
+                        while (tmp.Count != 0)
+                        {
+                            if (tmp.Peek().Equals("(")|| tmp.Peek().Equals("["))
+                            {
+                                break;
+                            }
+                            res.Add(tmp.Pop());
+                        }
+                        if (flag)
+                        {
+                            tmp.Push("-");
+                        }
+                        else {
+                            tmp.Push("OP_NEG");
+                            flag = true;
+                        }
+                        
+                    }
+                    else if (s.Name.Equals("*"))
+                    {
+                        while (tmp.Count != 0)
+                        {
+                            if (tmp.Peek().Equals("+") || tmp.Peek().Equals("-") || tmp.Peek().Equals("(") || tmp.Peek().Equals("["))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                res.Add(tmp.Pop());
+                            }
+                        }
+                        tmp.Push("*");
+                    }
+                    else if (s.Name.Equals("/"))
+                    {
+                        while (tmp.Count != 0)
+                        {
+                            if (tmp.Peek().Equals("+") || tmp.Peek().Equals("-") || tmp.Peek().Equals("(") || tmp.Peek().Equals("["))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                res.Add(tmp.Pop());
+                            }
+                        }
+                        tmp.Push("/");
+                    }
+                    else if (s.Name.Equals("["))
+                    {
+                        tmp.Push("[");
+                    }
+                    else if (s.Name.Equals("]")) {
+                        while (tmp.Count != 0 && !tmp.Peek().Equals("["))
+                        {
+                            res.Add(tmp.Pop());
+                        }
+                        res.Add("locArray");
+                        tmp.Pop();
+                    }
+                    else
+                    {
+                        res.Add(s.Name);
+                    }
                 }
+             return;
             }
             else {
-                foreach (Node n in s.Next) {
-                    get(n);
+                if (s.Type == TokenType.unary) {
+                    if (s.Next.Count == 2) {
+                        flag = false;
+                    }
+                
                 }
-            
+               
+                foreach (Node q in s.Next) {
+                    GeneratePostExpr(q);
+                }
+
             }
-            
         }
     }
 }
--- WordAnalyzer/WordAnalyzer/Program.cs
+++ WordAnalyzer/WordAnalyzer/Program.cs
@@ -7,10 +7,11 @@ namespace WordAnalyzer
     {
         static void Main(string[] args)
         {
-             Analyzer a = new Analyzer(@"test.txt");
+             Analyzer a = new Analyzer(@"C:\Users\tp103\Desktop\aaa.cmm");
             /* a.ListToFile();
              a.WordSetToFile();*/
             List<Token> b = a.toTokenList();
+            a.ListToFile();
             foreach (Token s in b) {
                 Console.WriteLine(s.LineNum+" "+s.Type+" "+s.Value);
             
